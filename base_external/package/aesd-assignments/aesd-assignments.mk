737669a8ed34dc2104b4fc065022a97ce41a51fb
stamrakar <stamrakar@Ubuntu.myguest.virtualbox.org>
2024-12-10 19:54:29 -0700

    adding the modprobes to S40network

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 # TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 34d67c3f9d0fbceb37c55e4d695fd21dce959610
+AESD_ASSIGNMENTS_VERSION = c7df678e0c598f56f0684d54651583d10ff89536
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
